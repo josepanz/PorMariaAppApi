acf848efec44968aa6183cd572be790e21156b34
jpanza <jpanza@bepsa.com.py>
2024-02-21 11:36:03 -0300

    Modify some values

--- src/test/java/com/pormaria/api/crud/CrudApplicationTests.java
+++ src/test/java/com/pormaria/api/crud/CrudApplicationTests.java
@@ -1,13 +1,13 @@
-//package com.pormaria.api.crud;
-//
-//import org.junit.jupiter.api.Test;
-//import org.springframework.boot.test.context.SpringBootTest;
-//
-//@SpringBootTest
-//class CrudApplicationTests {
-//
-//	@Test
-//	void contextLoads() {
-//	}
-//
-//}
+package com.pormaria.api.crud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
